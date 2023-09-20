bff5ded66e4f8154a741be06c651e8b8709637cd
jswesner <jeff.wesner@usd.edu>
2023-09-20 14:49:04 -0500

    randomize data for release

---
+++ code/simulate_salmon_data.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(janitor)
+
+# bring in original data
+fish_escapement_dont_post = read_csv("data/raw_data/fish_escapement.csv") 
+fish_mass_kgww_of_individual_fish_dont_post = read_csv("data/raw_data/fish_mass_kgww_of_individual_fish.csv")
+
+# save original data
+write_csv(fish_escapement_dont_post, file = "data/raw_data/fish_escapement_dont_post.csv")
+write_csv(fish_mass_kgww_of_individual_fish_dont_post, file = "data/raw_data/fish_mass_kgww_of_individual_fish_dont_post.csv")
+
+# randomize original data
+fish_escapement = fish_escapement_dont_post %>% 
+  pivot_longer(cols = c(-Year, -metric, -source)) %>%
+  mutate(sd = value/4,
+         value = abs(value + rnorm(nrow(.), 0, sd))) %>%   # add random noise to data
+  select(-sd) %>%
+  pivot_wider(names_from = name, values_from = value) %>% 
+  mutate(source = "Simulated data. Contact Dr. Greg Ruggerone for raw data")
+
+fish_mass_kgww_of_individual_fish = fish_mass_kgww_of_individual_fish_dont_post %>% 
+  pivot_longer(cols = c(-Year, -units, -Source)) %>%
+  mutate(sd = value/4,
+         value = abs(value + rnorm(nrow(.), 0, sd))) %>%   # add random noise to data
+  select(-sd) %>%
+  pivot_wider(names_from = name, values_from = value) %>% 
+  mutate(source = "Simulated data. Contact Dr. Greg Ruggerone for raw data")
+
+
+
+# save randomized file for models
+write_csv(fish_escapement, file = "data/raw_data/fish_escapement.csv")
+write_csv(fish_mass_kgww_of_individual_fish, file = "data/raw_data/fish_mass_kgww_of_individual_fish.csv")
